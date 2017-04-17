d1f4e4307734b74f8fb73fbdcf51ea5acc5ad902
Alexandra Holcombe and Caitlin Hines <alexandra+caitlin@>
2017-04-17 11:32:26 -0700

    Correctly link css file

--- src/WebApplication1/Startup.cs
+++ src/WebApplication1/Startup.cs
@@ -40,6 +40,8 @@ namespace ToDoList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
+
             //2
             app.UseMvc(routes =>
             {
