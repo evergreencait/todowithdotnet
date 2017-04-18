964d23276b9888db286be642174771bae8c5ddbb
Caitlin Hines and Keely Glenn <caitlin+keely@>
2017-04-18 09:22:28 -0700

    add categories

--- src/WebApplication1/Controllers/CategoriesController.cs
+++ src/WebApplication1/Controllers/CategoriesController.cs
@@ -4,62 +4,64 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
-    public class ItemsController : Controller
+    public class CategoryController : Controller
     {
         private ToDoListContext db = new ToDoListContext();
         public IActionResult Index()
         {
-            return View(db.Items.Include(items => items.Category).ToList());
+            return View(db.Categories.ToList());
         }
 
         public IActionResult Details(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-            return View(thisItem);
+            var thisCategory = db.Categories.FirstOrDefault(categories =>
+            categories.CategoryId == id);
+            return View(thisCategory);
         }
-        
+
         public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Item item)
+        public IActionResult Create(Category category)
         {
-            db.Items.Add(item);
+            db.Categories.Add(category);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public IActionResult Edit(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-            return View(thisItem);
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
         }
 
         [HttpPost]
-        public IActionResult Edit(Item item)
+        public IActionResult Edit(Category category)
         {
-            db.Entry(item).State = EntityState.Modified;
+            db.Entry(category).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-            return View(thisItem);
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
         }
 
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteConfirmed(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-            db.Items.Remove(thisItem);
+            var thisCategory = db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            db.Categories.Remove(thisCategory);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
